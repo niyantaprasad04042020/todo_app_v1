4c2f0fe9edb6ed27257fe2e442070f76de0473f4
NiyantaPrasad <niyanta@zwayam.com>
2022-04-12 12:51:21 +0530

    removing binding.pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   def signup
     @user = User.create(user_params)
     if @user.valid?
-      binding.pry
       UserMailer.signup_confirmation(@user).deliver
       token = encode_token({user_id: @user.id})
       render json: {user: @user, token: token}
